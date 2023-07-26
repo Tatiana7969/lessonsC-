f86e76102ad5828aec2e141d02e43dead9adbd4a
Tata_Babar <tbabarykina@gmail.com>
2023-07-26 22:04:10 +0300

    добавили 14 и16 задачи

---
+++ urok002/Zadacha14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая 
+// 1. Принимает на вход число 
+// 2. Проверяет, кратно ли оно 7 и 23 одновременно
+// 14 -> нет 
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = Miltiplicity(num);
+Console.WriteLine(result? "ДА": "Нет");
+
+bool Miltiplicity (int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+
+
+
+
---
+++ urok002/Zadacha16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая
+// 1. Принимает на вход 2 числа
+// 2. Проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет 
+
+// using System.Globalization;
+
+bool Square(int number1, int number2)
+{
+    return number1 == number2 * number2 || number2 == number1 * number1;
+}
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool result = Square(a, b);
+Console.WriteLine(result ? "Да": "Нет");
