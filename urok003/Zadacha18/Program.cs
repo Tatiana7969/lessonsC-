a8ddb1922e4837eb2526be02d8ba1e8f12da7e26
Tata_Babar <tbabarykina@gmail.com>
2023-07-31 23:38:33 +0400

    Задача с четвертями и диапазонами

---
+++ urok003/Zadacha18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая 
+// по заданному номеру четверти показывает диапазон 
+// возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine("Введите номер четверти: ");
+string numberQuarter = Console.ReadLine();
+
+string range = RangeQuarter(numberQuarter);
+if (range != null)
+    Console.WriteLine(range);
+else Console.WriteLine("Некорректная четверть");
+
+string RangeQuarter (string numberQuarter)
+{
+    if (numberQuarter == "1") return "x > 0 && y > 0";
+    if (numberQuarter == "2") return "x < 0 && y > 0";
+    if (numberQuarter == "3") return "x < 0 && y < 0";
+    if (numberQuarter == "4") return "x > 0 && y < 0";
+    return null; 
+}
+
+
+
