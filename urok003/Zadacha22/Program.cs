4aa3792cc8a0a81e23f78abae1565c13c7626815
Tata_Babar <tbabarykina@gmail.com>
2023-08-01 11:41:11 +0400

    Задача с квадратами числа

---
+++ urok003/Zadacha22/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N)
+// 2. и выдает таблицу квадратов чисел от 1 до N 
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1,4
+
+using System.Data;
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+    return;
+}
+SquareTable (number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int square = i * i;
+        Console.WriteLine($"{i}  {square}");
+    }
+}
+
+
+
+
+
+
