c86c2a36a857f13c9717a0752d5dffc680515431
Tata_Babar <tbabarykina@gmail.com>
2023-08-04 16:57:23 +0400

    Задача26 количество цифр в числе

--- urok004/Zadacha24/Program.cs
+++ urok004/Zadacha24/Program.cs
@@ -17,7 +17,12 @@ int SumNumbers(int num)
     int sum = 0;
     for (int i = 1; i <= num; i++)
     {
+        checked
+        {
          sum += i; //sum = sum + i
+        }
     }
     return sum;
+
+    
 }
---
+++ urok004/Zadacha26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число
+// 2. выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = CountNumberDigit(number);
+Console.WriteLine($"Количество цифр в числе {number} = {result}");
+
+int CountNumberDigit(int num)
+{
+    int countDigit = 0;
+    while (num > 0 || num < 0)
+    {
+        countDigit++;
+        num = num / 10;
+    }
+    return countDigit;
+}
