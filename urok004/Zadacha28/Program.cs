7eeb086e8dac573408dd10f3aef956c08b1c03b8
Tata_Babar <tbabarykina@gmail.com>
2023-08-04 17:31:32 +0400

    Задача28 произвдение чисел от 1 доN

---
+++ urok004/Zadacha28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая
+// 1. принимает на вход число N 
+// 2. выдает произведение чисел от 1 до N 
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());   
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i < num; i++)
+    {
+        checked
+        {
+         fact *= i; //fact = fact * i
+        }
+    }
+    return fact;
+}
