3abfcc4212c580d5b20eb0201d73db507003a145
Tata_Babar <tbabarykina@gmail.com>
2023-08-09 19:43:00 +0400

    Задача32массивб инверсия знаков отр и полщжит

---
+++ urok005/Zadach32/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу замена элементов
+// массива: положительные элементы замените
+// на соответствующие отрицательные и наоборот
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size] ;
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+ void PrintArray(int[]arr)
+ {
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, "); 
+        else Console.Write($"{arr[i]}");   
+    }
+    Console.Write("]");
+ }
+
+void InvertionArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+
+}
+ int[] array = CreateArrayRndInt(12, -9, 9);
+ PrintArray(array); 
+ Console.WriteLine();
+ InvertionArray(array);
+ PrintArray(array);
+ Console.WriteLine();
